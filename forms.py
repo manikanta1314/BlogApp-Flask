4a295655a9edd83b0398cb6c3ddcf5fcf989a896
Pallapothu Manikanta <pallapothum784@gmail.com>
2024-12-13 14:45:08 +0530

    Resolved merge conflicts

--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 # auto converting python classes into html forms.
 from wtforms import StringField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms.validators import DataRequired,Length,Email,EqualTo,Password
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username",
                            validators=[DataRequired(), Length(min = 2,max = 20)])
     email = StringField("Email",
-                        validators=[DataRequired(),Email()])
+                        validators=[DataRequired(),Email()]) 
+    
+    password = Pa
